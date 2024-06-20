29ad92addfdf137b5fd1fbd59dbc599d41f1b671
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2024-06-20 17:20:09 +0100

    jquery

--- 8/script.js
+++ 8/script.js
@@ -1,3 +1,10 @@
 const newFunction = () => {
-  document.getElementById("demo").innerHTML = "Changed";
+  let num = 0;
+  document.getElementById("demo").innerHTML = num += 5;
+};
+
+const newFunction1 = () => {
+  $("#first_h2").attr("style", "color:red").html("Favorite Colors");
+
+
 };
